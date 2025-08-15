d368301d505e03488b1f833309da6cc1cf5d8615
DeepMajumderr <majumderdeep06@gmail.com>
2025-08-15 13:52:18 +0530

    Day 5: (Completed Signin,and forgot password ui)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
+import ForgotPassword from './pages/ForgotPassword'
 export const serverUrl = "http://localhost:8000"
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/signup' element={<Signup /> } />
-      <Route path='/signin' element={<Signin /> } />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
     </Routes>
   )
 }
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,8 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo2 from "../assets/logo2.png"
+import white_logo from "../assets/white_logo.png"
+import { IoEye } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa";
+import axios from 'axios'
+import { serverUrl } from '../App';
+import { ClipLoader } from "react-spinners"
+import { useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+
+  const [inputClicked, setinputClicked] = useState({
+    userName: false,
+    password: false
+  })
+
+  const [showPassword, setshowPassword] = useState(false)
+  const [userName, setuserName] = useState("")
+  const [password, setpassword] = useState("")
+  const [loading, setloading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleSignIn = async () => {
+    setloading(true)
+    try {
+      const result = await axios.post(`${serverUrl}/api/auth/signin`,
+        { userName, password },
+        { withCredentials: true }
+      )
+      console.log(result.data)
+      setloading(false)
+    } catch (error) {
+      console.log(error)
+      setloading(false)
+    }
+  }
+
+
   return (
-    <div>signin</div>
+    <div className='w-full h-screen bg-gradient-to-b from-black to-gray-900
+    flex flex-col justify-center items-center'>
+      <div className='w-[80%] lg:max-w-[60%] h-[535px]
+      bg-white rounded-2xl flex justify-center items-center
+      overflow-hidden border-2 border-[#1a1f23]'>
+
+        <div className='w-full lg:w-[50%] h-full bg-white flex
+        flex-col items-center justify-center p-[10px] gap-[20px]'>
+
+          <div className='flex gap-[10px] items-center text-[20px]
+          font-semibold mt-[40px]'>
+            <span>Sign In To</span>
+            <img src={logo2} alt="" className='w-[70px]' />
+          </div>
+
+
+          <div className='relative flex items-center justify-start w-[90%] 
+          h-[50px] rounded-2xl  border-2 border-black'
+            onClick={() => setinputClicked({ ...inputClicked, userName: true })}>
+
+            <label htmlFor='userName' className={`text-gray-700 absolute left-[20px]
+             p-[5px] bg-white text-[15px] ${inputClicked.userName ? "top-[-15px]" : ""}`}>
+              Enter Username
+            </label>
+            <input onChange={(e) => setuserName(e.target.value)}
+              value={userName}
+              type="text" id='userName' className='w-[100%] h-[100%]
+              rounded-2xl px-[20px] outline-none border-0 required' />
+          </div>
+
+          <div className='relative flex items-center justify-start w-[90%] 
+          h-[50px] rounded-2xl  border-2 border-black'
+            onClick={() => setinputClicked({ ...inputClicked, password: true })}>
+
+            <label htmlFor='password' className={`text-gray-700 absolute left-[20px]
+             p-[5px] bg-white text-[15px] ${inputClicked.password ? "top-[-15px]" : ""}`}>
+              Enter Password
+            </label>
+            <input onChange={(e) => setpassword(e.target.value)}
+              value={password}
+              type={showPassword ? "text" : "password"} id='password' className='w-[100%] h-[100%]
+              rounded-2xl px-[20px] outline-none border-0 required' />
+            {
+              showPassword ?
+                <FaEyeSlash
+                  onClick={() => setshowPassword(!showPassword)}
+                  className='absolute cursor-pointer right-[20px] w-[25px] h-[25px]' />
+                :
+                <IoEye
+                  onClick={() => setshowPassword(!showPassword)}
+                  className='absolute cursor-pointer right-[20px] w-[25px] h-[25px]' />
+            }
+
+
+          </div>
+
+          <div className='w-[90%] px-[20px] cursor-pointer' onClick={() => navigate("/forgot-password")}>
+            Forgot password ? Click here
+          </div>
+
+          <button className='w-[70%] px-[20px] py-[10px] bg-black text-white
+            font-semibold h-[50px] cursor-pointer rounded-2xl mt-[20px]'
+            onClick={handleSignIn} disabled={loading}>
+            {loading ? <ClipLoader size={30} color='white' /> : "Sign In"}
+          </button>
+
+          <p
+            onClick={() => navigate("/signup")}
+            className='cursor-pointer text-gray-800'>
+            Create A New Account ? 
+            <span className='border-b-2 border-b-black pb-[3px] text-black'>
+              {' '}Sign Up
+            </span>
+          </p>
+
+        </div>
+
+        <div className='md:w-[50%] h-full hidden lg:flex
+        justify-center items-center bg-[#000000] flex-col gap-[10px]
+        text-white text-[16px] font-semibold rounded-l-[30px]
+        shadow-2xl shadow-black'>
+          <img src={white_logo} alt="" className='w-[40%]' />
+          <p>
+            Not Just A Platform, It's a VYBE
+          </p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
